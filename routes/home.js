dcef085cb7294280eb4f9c4404efbe4b3ef63f0f
Lucas Martins Menezes <lucasmartinsmenezes@gmail.com>
2022-10-18 15:23:57 -0300

    add-a-book form

--- routes/home.js
+++ routes/home.js
@@ -2,8 +2,10 @@ const express = require('express')
 
 const router = express.Router()
 
+const bookAttributes = require ('../public/json/book-attributes.json')
+
 router.get('/', (req, res) => {
-  res.render('home', { pageTitle: 'Home', route: '/' })
+  res.render('home', { pageTitle: 'Home', route: '/', bookAttributes: bookAttributes })
 })
 
 module.exports = router
