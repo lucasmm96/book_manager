a91f85398b8137095cf12121c4cff35a13042175
Lucas Martins Menezes <lucasmartinsmenezes@gmail.com>
2022-10-18 09:07:30 -0300

    split head into include

--- routes/404.js
+++ routes/404.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 router.use((req, res) => {
-  res.render('404')
+  res.render('404', { pageTitle: '404' })
 })
 
 module.exports = router
--- routes/book.js
+++ routes/book.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 router.use('/book', (req, res) => {
-  res.render('book')
+  res.render('book', { pageTitle: 'Book' })
 })
 
 module.exports = router
