651a777e6fbabef72bd0159135e803da0c85d994
Lucas Martins Menezes <lucasmartinsmenezes@gmail.com>
2022-10-18 08:14:52 -0300

    set style

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express')
-
 const app = express()
+
+const path = require('path')
 
 const bookRoute = require('./routes/book')
 const homeRoute = require('./routes/home')
 const notFoundRoute = require('./routes/404')
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(bookRoute)
 app.use(homeRoute)
