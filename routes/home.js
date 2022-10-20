7cf85a9d8d7c5138abe7596b3870ec7a8f10baaf
Lucas Martins Menezes <lucasmartinsmenezes@gmail.com>
2022-10-20 16:00:07 -0300

    add book "storage"

--- routes/home.js
+++ routes/home.js
@@ -4,8 +4,16 @@ const router = express.Router()
 
 const bookAttributes = require ('../public/json/book-attributes.json')
 
+const books = []
+
 router.get('/', (req, res) => {
   res.render('home', { pageTitle: 'Home', route: '/', bookAttributes: bookAttributes })
 })
 
-module.exports = router
+router.post('/add-book', (req, res) => {
+  books.push(JSON.parse(JSON.stringify(req.body)));
+  res.redirect('/book')
+})
+
+exports.router = router
+exports.books = books
