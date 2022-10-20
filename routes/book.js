697b7ab7b19caadec66bdb5b469db27b55be682d
Lucas Martins Menezes <lucasmartinsmenezes@gmail.com>
2022-10-20 16:00:35 -0300

    read and send book data to be renderized

--- routes/book.js
+++ routes/book.js
@@ -2,8 +2,10 @@ const express = require('express')
 
 const router = express.Router()
 
+const home = require('./home')
+
 router.use('/book', (req, res) => {
-  res.render('book', { pageTitle: 'Book', route: '/book' })
+  res.render('book', { pageTitle: 'Book', route: '/book', bookList: home.books })
 })
 
 module.exports = router
