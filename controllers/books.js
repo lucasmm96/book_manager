9075a13d2aff211091c816a80b1453c7e07f54e6
Lucas Martins Menezes <lucasmartinsmenezes@gmail.com>
2022-10-24 17:39:00 -0300

    organize controller methods

--- controllers/books.js
+++ controllers/books.js
@@ -5,6 +5,19 @@ exports.getHome = (req, res) => {
   res.render('home', { pageTitle: 'Home', route: '/' });
 };
 
+exports.getBookList = (req, res) => {
+  Book.fetchAll(books => {
+    res.render('books/book-list', { pageTitle: 'Book List', route: '/book', bookList: books });
+  });
+};
+
+exports.getBookDetail = (req, res) => {
+  const bookId = req.params.bookId;
+  Book.findById(bookId, book => {
+    res.render('books/book-detail', { pageTitle: 'Book Detail', route: '/book', book: book });
+  });
+};
+
 exports.getAddBook = (req, res) => {
   res.render('books/book-add', { pageTitle: 'Add Book', route: '/book', bookAttributes: bookAttr });
 };
@@ -19,17 +32,4 @@ exports.postAddBook = (req, res) => {
   const book = new Book(title, author, added_at, finished_at, score, status);
   book.save();
   res.redirect('/');
-};
-
-exports.getBookList = (req, res) => {
-  Book.fetchAll(books => {
-    res.render('books/book-list', { pageTitle: 'Book List', route: '/book', bookList: books });
-  });
-};
-
-exports.getBookDetail = (req, res) => {
-  const bookId = req.params.bookId;
-  Book.findById(bookId, book => {
-    res.render('books/book-detail', { pageTitle: 'Book Detail', route: '/book', book: book });
-  });
 };
