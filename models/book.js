0992c9dd9fa9f101db839bf6592a3131eb452dfa
Lucas Martins Menezes <lucasmartinsmenezes@gmail.com>
2022-10-27 15:44:54 -0300

    set table structure for sequelize

--- models/book.js
+++ models/book.js
@@ -1,39 +1,19 @@
-const db = require('../util/database')
+const Sequelize = require('sequelize');
+const sequelize = require('../util/database');
 
-module.exports = class Book {
-  constructor(id, title, author, added_at, finished_at, score, status) {
-    this.id = id;
-    this.title = title;
-    this.author = author;
-    this.added_at = added_at;
-    this.finished_at = finished_at;
-    this.score = score;
-    this.status = status;
-  };
+const Book = sequelize.define('book', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  title: Sequelize.STRING,
+  author: Sequelize.STRING,
+  added_at: Sequelize.DATE,
+  finished_at: Sequelize.DATE,
+  score: Sequelize.DOUBLE,
+  status: Sequelize.STRING,
+});
 
-  save() {
-    if(this.id) {
-      return db.execute(
-        'UPDATE node_course.book SET title = ?, author = ?, added_at = ?, finished_at = ?, score = ?, status = ? WHERE id = ?',
-        [this.title, this.author, this.added_at, this.finished_at, this.score, this.status, this.id]
-      );
-    } else {
-      return db.execute(
-        'INSERT INTO node_course.book (title, author, added_at, finished_at, score, status) VALUES (?, ?, ?, ?, ?, ?)',
-        [this.title, this.author, this.added_at, this.finished_at, this.score, this.status]
-      );
-    }
-  };
-
-  static remove(id) {
-    return db.execute('DELETE FROM node_course.book WHERE id = ?', [id]);
-  };
-
-  static fetchAll() {
-    return db.execute('SELECT * FROM node_course.book;');
-  };
-
-  static findById(id) {
-    return db.execute('SELECT * FROM node_course.book WHERE id = ?', [id]);
-  };
-}
+module.exports = Book;
