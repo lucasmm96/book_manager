3eb9bddf65a885d1d6dc6faf0531b6e21a17bea2
Lucas Martins Menezes <lucasmartinsmenezes@gmail.com>
2022-10-27 15:45:48 -0300

    create table via sequelize when it doesn't exist

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const path = require('path');
 const bookRoute = require('./routes/book');
 const homeRoute = require('./routes/home');
 const notFoundRoute = require('./routes/404');
+const sequelize = require('./util/database')
 
 app.set('view engine', 'pug');
 app.set('views', 'views');
@@ -18,4 +19,8 @@ app.use(bookRoute);
 app.use(homeRoute);
 app.use(notFoundRoute);
 
-app.listen(3000);
+sequelize.sync()
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch(err => console.log(err));
