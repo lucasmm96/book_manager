a4ebe7aaf8e4a749e5ec04f80c8b9036f2c04e42
Lucas Martins Menezes <lucasmartinsmenezes@gmail.com>
2022-10-29 17:01:48 -0300

    create pool to keep the connection opened

--- util/database.js
+++ util/database.js
@@ -1,15 +1,27 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = callback => {
   MongoClient.connect('mongodb+srv://lucasma:vhftoJv2cCYWdg9C@cluster0.y7lomn8.mongodb.net/?retryWrites=true&w=majority')
   .then(client => {
-    console.log('Successfully Connected!');
-    callback(client);
+    console.log('Successfully Connected to db!');
+    _db = client.db();
+    callback();
   })
   .catch(err => {
     console.log(err);
+    throw err;
   });
-}
+};
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No db found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
