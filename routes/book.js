21640900ef5f978dea717e29703f2509dd650fee
Lucas Martins Menezes <lucasmartinsmenezes@gmail.com>
2022-10-29 17:02:34 -0300

    comment out routes temporarily

--- routes/book.js
+++ routes/book.js
@@ -2,18 +2,18 @@ const express = require('express');
 const router = express.Router();
 const booksController = require('../controllers/books');
 
-router.get('/book/list', booksController.getBookList);
+// router.get('/book/list', booksController.getBookList);
 
-router.get('/book/detail/:bookId', booksController.getBookDetail);
+// router.get('/book/detail/:bookId', booksController.getBookDetail);
 
 router.get('/book/add', booksController.getAddBook);
 
 router.post('/add-book', booksController.postAddBook);
 
-router.get('/book/edit/:bookId', booksController.getEditBook);
+// router.get('/book/edit/:bookId', booksController.getEditBook);
 
-router.post('/edit-book', booksController.postEditBook);
+// router.post('/edit-book', booksController.postEditBook);
 
-router.get('/book/remove/:bookId', booksController.getRemoveBook);
+// router.get('/book/remove/:bookId', booksController.getRemoveBook);
 
 module.exports = router;
