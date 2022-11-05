80f56db8171846549b696d41169b36a32fcfa5d9
Lucas Martins Menezes <lucasmartinsmenezes@gmail.com>
2022-11-05 18:23:24 -0300

    rename from books to admin

---
+++ routes/admin.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const adminController = require('../controllers/admin');
+
+router.get('/admin/home', adminController.getHome);
+
+router.get('/admin/manage-book', adminController.getBookList);
+
+router.get('/admin/manage-book/add', adminController.getAddBook);
+
+router.post('/admin/add-book', adminController.postAddBook);
+
+router.get('/admin/manage-book/edit/:bookId', adminController.getEditBook);
+
+router.post('/admin/edit-book', adminController.postEditBook);
+
+router.get('/admin/manage-book/delete/:bookId', adminController.getDeleteBook);
+
+module.exports = router;
+
