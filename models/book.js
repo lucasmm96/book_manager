33bbd61925ff3849b8b54217fefbf0e9ef97a170
Lucas Martins Menezes <lucasmartinsmenezes@gmail.com>
2022-11-08 09:20:29 -0300

    remove commented out fields

--- models/book.js
+++ models/book.js
@@ -4,10 +4,6 @@ const Schema = mongoose.Schema;
 const bookSchema = new Schema({
 	title: { type: String, required: true },
 	author: { type: String, required: true }
-	// addedAt: { type: Date, required: true },
-	// finishedAt: { type: Date, required: false },
-	// score: { type: Number, required: false },
-	// status: { type: String, required: false }
 });
 
 module.exports = mongoose.model('Book', bookSchema);
