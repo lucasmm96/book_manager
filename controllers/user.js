d2ed721f09a9f6ee6d324961bed35790eec196f0
Lucas Martins Menezes <lucasmartinsmenezes@gmail.com>
2022-11-08 16:02:12 -0300

    add book controller methods

--- controllers/user.js
+++ controllers/user.js
@@ -5,21 +5,22 @@ exports.getHome = (req, res) => {
 	res.render('user/home', { pageTitle: 'User', route: '/user' });
 };
 
-exports.getUserBook = (req, res) => {
-	User.findById('6367e5905de87f7310c03a69')
-		.then(user => {
-			const bookList = user.book.items;
-			res.render('user/book-list', { pageTitle: 'Book List', route: '/user', bookList: bookList });
+exports.getBookList = (req, res) => {
+	Book.find()
+		.then(books => {
+			res.render('user/book-list', { pageTitle: 'Book List', route: '/user', bookList: books, filter: 'all' });
 		})
 		.catch(err => console.log(err));
 };
 
-exports.getBookList = (req, res) => {
-	Book.find()
-		.then(bookList => {
-			res.render('user/book-list', { pageTitle: 'Book List', route: '/user', bookList: bookList });
-		})
-		.catch(err => console.log(err));
+exports.getUserBook = (req, res) => {
+	req.user
+	.populate('books._id')
+	.then(user => {
+		const books = user.books;
+		res.render('user/book-list', { pageTitle: 'Book List', route: '/user', bookList: books, filter: 'user' });
+	})
+	.catch(err => console.log(err));
 };
 
 exports.getAddBook = (req, res) => {
@@ -33,35 +34,19 @@ exports.getAddBook = (req, res) => {
 
 exports.postAddBook = (req, res) => {
 	const bookId = req.body.id;
-	const AddedAt = new Date().toISOString().slice(0, 10);
-	const FinishedAt = req.body.finishedAt;
-	const Score = req.body.score;
-	const Status = req.body.status;
-	
-	User.findById('6367e5905de87f7310c03a69')
-		.then(user => {
-			const book = {
-				_id: bookId,
-				addedAt: AddedAt,
-				finishedAt: FinishedAt,
-				score: Score,
-				status: Status
-			}
-			user.book.items.push(book);
-			return user.save();
-		})
-		.then(() => {
-			res.redirect('/user/book/add');
-		})
-		.catch(err => console.log(err));	
-};
+	const addedAt = new Date();
+	const finishedAt = req.body.finishedAt;
+	const score = req.body.score;
+	const status = req.body.status;
 
-exports.getBookDetail = (req, res) => {
-	const bookId = req.params.bookId;
 	Book.findById(bookId)
-		.then(row => {
-			console.log(row);
-			res.render('user/book-detail', { pageTitle: 'Book Detail', route: '/user', book: row });
-		})
-		.catch(err => console.log(err));
+	.then(book => {
+		console.log(book);
+		console.log(req.user);
+		return req.user.addBook(book, addedAt, finishedAt, score, status);
+	})
+	.then(() => {
+		res.redirect('/user/book/add');
+	})
+	.catch(err => console.log(err));
 };
