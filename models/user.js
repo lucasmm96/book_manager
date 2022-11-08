a09dc536c6f14f792a9eb47fc25ba1a79ceb1e23
Lucas Martins Menezes <lucasmartinsmenezes@gmail.com>
2022-11-08 09:21:07 -0300

    adjust array format

--- models/user.js
+++ models/user.js
@@ -4,17 +4,15 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
 	username: { type: String, required: true },
 	email: { type: String, required: true },
-	book: {
-		items: [
-			{
-				_id: { type: Schema.Types.ObjectId, required: true },
-				addedAt: { type: Date, required: true },
-				finishedAt: { type: Date, required: false },
-				score: { type: Number, required: false },
-				status: { type: String, required: false }
-			}
-		]
-	} 
+	books: [
+		{
+			_id: { type: Schema.Types.ObjectId, ref: 'Book', required: true },
+			addedAt: { type: Date, required: true },
+			finishedAt: { type: Date, required: false },
+			score: { type: Number, required: false },
+			status: { type: String, required: false }
+		}
+	]
 });
 
 module.exports = mongoose.model('User', userSchema);
