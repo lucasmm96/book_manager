2f4fbb167629822bafa37ec674d793790d8f1966
Lucas Martins Menezes <lucasmartinsmenezes@gmail.com>
2022-11-10 17:01:03 -0300

    use session module

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,6 @@
 exports.getLogin = (req, res) => {
-	const isLogged = req.get('Cookie').trim().split('=')[1] === 'true';
 	res.render('auth/login', {
 		pageTitle: 'Login',
-		route: '/login',
-		isAuthenticated: isLogged
+		route: '/login'
 	});
 };
--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,7 @@ const authController = require('../controllers/auth')
 router.get('/login', authController.getLogin);
 
 router.post('/user-login', (req, res) => {
-  res.setHeader('Set-Cookie', 'loggedIn=true; HttpOnly')
+  req.session.isLoggedIn = true;
   res.redirect('/');
 });
 
