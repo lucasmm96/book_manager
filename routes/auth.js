2b8f023e0e9aa712bc4d3320a06cb45b24d9738a
Lucas Martins Menezes <lucasmartinsmenezes@gmail.com>
2022-11-10 20:28:38 -0300

    split login/logout methods

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/auth')
-const User = require('../models/user');
 
 router.get('/login', authController.getLogin);
 
-router.post('/user-login', (req, res) => {
-  User.findById('636a48f4645ec17fa5c13a10')
-    .then(user => {
-      req.session.user = user;
-      req.session.isLoggedIn = true;
-      res.redirect('/');
-    })
-    .catch(err => console.log(err));
-});
+router.post('/user-login', authController.postLogin)
+
+router.post('/user-logout', authController.postLogout);
 
 module.exports = router;
