2a7e57c35044716730afa092edb9bc81c7e60aa3
Lucas Martins Menezes <lucasmartinsmenezes@gmail.com>
2022-11-10 13:45:56 -0300

    setup authentication cookie

--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,7 @@ const authController = require('../controllers/auth')
 router.get('/login', authController.getLogin);
 
 router.post('/user-login', (req, res) => {
-  req.isLogged = true;
+  res.setHeader('Set-Cookie', 'isAuthenticated=true')
   res.redirect('/');
 });
 
