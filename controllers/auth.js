98c713236955615dfdecf257e680e36274b780e6
Lucas Martins Menezes <lucasmartinsmenezes@gmail.com>
2022-11-10 20:27:23 -0300

    post login/logout

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,25 @@
+const User = require('../models/user');
+
 exports.getLogin = (req, res) => {
 	res.render('auth/login', {
 		pageTitle: 'Login',
-		route: '/login'
+		route: '/login',
+		isAuthenticated: req.session.isLoggedIn
 	});
+};
+
+exports.postLogin = (req, res) => {
+	User.findById('636a48f4645ec17fa5c13a10')
+		.then(user => {
+			req.session.user = user;
+			req.session.isLoggedIn = true;
+			res.redirect('/');
+		})
+		.catch(err => console.log(err));
+};
+
+exports.postLogout = (req, res) => {
+	req.session.destroy(() => {
+		res.redirect('/login');
+	})
 };
