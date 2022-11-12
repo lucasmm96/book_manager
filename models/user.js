defcbe1d396cb31e32b55259e816d7c5aa38a7ed
Lucas Martins Menezes <lucasmartinsmenezes@gmail.com>
2022-11-12 17:08:04 -0300

    add password to user schema

--- models/user.js
+++ models/user.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
 	username: { type: String, required: true },
 	email: { type: String, required: true },
+	password: { type: String, required: true  },
 	books: [
 		{
 			id: { type: Schema.Types.ObjectId, ref: 'Book', required: true },
