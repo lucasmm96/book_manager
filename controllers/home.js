332d52ccb3533a491a8514c4b54770fe3b0a29e5
Lucas Martins Menezes <lucasmartinsmenezes@gmail.com>
2022-11-12 16:19:30 -0300

    Change home page name

--- controllers/home.js
+++ controllers/home.js
@@ -1,6 +1,6 @@
 exports.getHome = (req, res) => {
 	if (!req.session.isLoggedIn) { return res.render('auth/login', { pageTitle: 'Login', pageInfo: 'Login', route: '/login' }) }
-	res.render('home', { pageTitle: 'Home',
+	res.render('home', { pageTitle: 'Book Manager',
 		route: '/',
 		pageInfo: `Hi ${req.user.username}! Welcome to Book Manager.`,
 		isAuthenticated: req.session.isLoggedIn});
