a1ffd6b929c75b3c742a65054131837738855716
Lucas Martins Menezes <lucasmartinsmenezes@gmail.com>
2022-11-13 13:38:22 -0300

    log error

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,5 +1,6 @@
 module.exports = (req, res, next) => {
   if (!req.session.isLoggedIn) {
+    console.log('middleware/is-auth.js: You are not logged in.');
     return res.redirect('/login');
   }
   next();
